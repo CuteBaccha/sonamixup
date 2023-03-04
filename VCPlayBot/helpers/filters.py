f58850bb585ba996cf8d8b62ffc5b18c0525be1c
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-03-04 12:49:32 +0530

    Update filters.py

--- VCPlayBot/helpers/filters.py
+++ VCPlayBot/helpers/filters.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from pyrogram import filters
 
-from VCPlayBot.config import COMMAND_PREFIXES
+from sonamixup.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
